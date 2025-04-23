ad7e05c6fac67ab878428d0f5056038c8192af54
Vamshika-7 <mittakolavamshika@gmail.com>
2025-04-23 22:32:33 +0530

    Update part1.cpp

--- part1.cpp
+++ part1.cpp
@@ -4,9 +4,10 @@
 using namespace std;
 class student{
 	private:
-		
+		string name,roll_no,course,address,email_id,contact_no;
 	public:
 		void menu();
+		void insert();
 };
 void student::menu(){
 	int choice;
@@ -21,6 +22,41 @@ void student::menu(){
 	cout<<"5. Delete Record"<<endl;
 	cout<<"6. Exit"<<endl;
 	
+	cout<<"\t\t\t---------------------------"<<endl;
+	cout<<"\t\t\tChoose Option:[1/2/3/4/5/6]"<<endl;
+	cout<<"\t\t\t---------------------------"<<endl;
+	cout<<"Enter your choice: ";
+	cin>>choice;
+
+	switch(choice){
+		case 1:
+			insert();
+			break;
+		default:
+			cout<<"\n\t\t\t Invalid Choice...Please Try Again...";
+			break;
+
+	}
+}
+void student::insert(){
+	system("cls");
+	fstream file;
+	cout<<"\n-------------------Add Student Details-------------------"<<endl;
+	cout<<"\t\tEnter Name: ";
+	cin>>name;
+	cout<<"\t\tEnter Roll Number: ";
+	cin>>roll_no;
+	cout<<"\t\tEnter Course: ";
+	cin>>course;
+	cout<<"\t\tEnter Email ID: ";
+	cin>>email_id;
+	cout<<"\t\tEnter Contact Number: ";
+	cin>>contact_no;
+	cout<<"\t\tEnter Address: ";
+	cin>>address;
+	cout<<"\t\tEnter Name: ";
+	cin>>name;
+
 }
 int main(){
 	student project;
